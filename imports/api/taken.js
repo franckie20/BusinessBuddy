f7925409195d7af16f0add06d7cd7ea1d0080ee7
franckie20 <franck.verschuur@gmail.com>
2016-06-30 22:50:49 +0200

    Progress afspraken & taken

--- imports/api/taken.js
+++ imports/api/taken.js
@@ -14,12 +14,61 @@ Meteor.methods({
             throw new Meteor.Error('not-authorized');
         }
 
-        Taken.insert({
-        });
+        if(Taken.find({Titel: data.Titel}, {Omschrijving: data.Omschrijving}).count() == 0) {
+            Taken.insert({
+                Titel: data.Titel,
+                Omschrijving: data.Omschrijving,
+                Einddatum: data.Einddatum,
+                Eindtijd: data.Eindtijd,
+                Uitvoerder: {
+                    _id: data.Uitvoerder._id,
+                    username: data.Uitvoerder.username,
+                    profile: {
+                        name: data.Uitvoerder.profile.name
+                    }
+                },
+                Reminder: {
+                    datum: data.Reminder.datum,
+                    tijd: data.Reminder.tijd
+                },
+                Urgentie: {
+                    naam: data.Urgentie.naam,
+                    kleur: data.Urgentie.kleur
+                }
+            });
+        }
     },
     'taken.remove' (taak) {
         Taken.remove({
             '_id': taak._id
+        });
+    },
+
+    'taken.update' (details) {
+        Taken.update({
+            _id: details._id
+        }, {
+            $set: {
+                'Titel': details.Titel,
+                'Omschrijving': details.Omschrijving,
+                'Einddatum': details.Einddatum,
+                'Eindtijd': details.Eindtijd,
+                'Uitvoerder': {
+                    '_id': details.Uitvoerder._id,
+                    'username': details.Uitvoerder.username,
+                    'profile': {
+                        'name': details.Uitvoerder.profile.name
+                    }
+                },
+                'Reminder': {
+                    'datum': details.Reminder.datum,
+                    'tijd': details.Reminder.tijd
+                },
+                'Urgentie': {
+                    'naam': details.Urgentie.naam,
+                    'kleur': details.Urgentie.kleur
+                }
+            }
         });
     }
 });
