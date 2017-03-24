7d8b7ece402381f010f7bae18e11a560b86bee37
franckie20 <franck.verschuur@gmail.com>
2017-03-24 08:17:13 +0100

    Backend code

--- imports/api/urgentie.js
+++
@@ -1,8 +0,0 @@
-Urgentie = new Mongo.Collection('urgentie');
-
-if (Meteor.isServer) {
-    // This code only runs on the server
-    Meteor.publish('urgentie', function() {
-        return Urgentie.find();
-    });
-}
--- imports/api/users.js
+++
@@ -1,6 +0,0 @@
-if (Meteor.isServer) {
-    // This code only runs on the server
-    Meteor.publish('users', function() {
-        return Meteor.users.find({}, {fields:{profile: true, emails: true, username: true}});
-    });
-}
